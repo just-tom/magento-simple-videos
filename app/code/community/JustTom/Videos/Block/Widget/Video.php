5e6f63c25ac272821e8d01bf622ceb151ab04e2c
Cranbri Web Solutions <cranbriwebsolutions@gmail.com>
2015-06-18 21:04:24 +0100

    added widget files

--- app/code/community/JustTom/Videos/Block/Widget/Video.php
+++ app/code/community/JustTom/Videos/Block/Widget/Video.php
@@ -4,5 +4,31 @@ class JustTom_Videos_Block_Widget_Video
     extends Mage_Core_Block_Template
     implements Mage_Widget_Block_Interface
 {
+    public function _construct()
+    {
+        if (!$this->hasTemplate()) {
+            $this->setTemplate('justtom/widget/video.phtml');
+        }
+        parent::_construct();
+    }
 
+    public function isEnabled()
+    {
+        return ($this->getData('enabled') !== 'false') ? true : false;
+    }
+
+    public function getVideoWidth()
+    {
+        return ($this->getData('video_width') != NULL) ? $this->getData('video_width') : 600;
+    }
+
+    public function getVideoHeight()
+    {
+        return ($this->getData('video_height') != NULL) ? $this->getData('video_height') : 400;
+    }
+
+    public function getEmbedUrl()
+    {
+        return 'https://www.youtube.com/embed/' . $this->getData('youtube_code');
+    }
 }
